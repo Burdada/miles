76e751059aec5c272120a3af939e13fb8d8ace55
Daria Burdakova <d.saharova@inbox.ru>
2022-05-30 19:53:05 +0300

    BMS added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 80_000;
-        int miles = amount / 20;
-        System.out.println("Начисленные мили: " + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
