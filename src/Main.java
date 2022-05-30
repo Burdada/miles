3186ddf1bf403a2e63db16bb5845b984250e727e
Daria Burdakova <d.saharova@inbox.ru>
2022-05-30 08:11:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 80_000;
+        int miles = amount / 20;
+        System.out.println("Начисленные мили: " + miles);
+    }
+}
